3a86d63bbfae52ed3aa861a1d1ce13666be62df5
Eamonn Maguire <eamonnmag@gmail.com>
2013-09-10 11:22:46 +0100

    Now big data to big bollocks

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bBig Data\b/g, "Big Bollocks");
+	v = v.replace(/\bBig data\b/g, "Big bollocks");
+	v = v.replace(/\bbig Data\b/g, "big Bollocks");
+	v = v.replace(/\big data\b/g, "big bollocks");
 	
 	textNode.nodeValue = v;
 }
